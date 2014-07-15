2e8d29b54ab199c1a3c78842c1569d6c79b05f4e
Andy Shi <andyshi@college.harvard.edu>
2014-07-15 14:29:16 -0400

    fixed bugs in sam parsing, wrote slurm driver script

--- parseSams.py
+++ parseSams.py
@@ -5,25 +5,25 @@ import numpy as np
 import constants_knead_data as const
 import re
 
-ORIG_ENDINGS = ['.fir.fastq', '.sec.fastq']
+ORIG_ENDINGS = ['.fir.fastq']
 TRIM_ENDINGS = ['_pe.sam', '_se_1.sam', '_se_2.sam']
 NOTRIM_ENDING = '_pe_notrim.sam'
 
 def getFiles(strDataset, strParentDir, strAligner, fTrimmed):
     ''' Get file names we need to count '''
-    orig_fnames = [os.path.join(strParentDir, strDataset + ending) for ending in
-            ORIG_ENDINGS]    
+    orig_fnames = [os.path.join(strParentDir, strDataset, strDataset + ending)
+            for ending in ORIG_ENDINGS]    
 
     trimmed_fnames = []
     sam_fnames = []
     if fTrimmed:
-        trimmed_fnames = [os.path.join(strParentDir, strDataset + ending) for ending
-                in const.TRIM_PE_ENDINGS]
-        sam_fnames = [os.path.join(strParentDir, strDataset + "-" + strAligner +
-            ending) for ending in const.TRIM_ENDINGS]
+        trimmed_fnames = [os.path.join(strParentDir, strDataset, strDataset +
+                ".out" + ending) for ending in const.TRIM_PE_ENDINGS[1:]]
+        sam_fnames = [os.path.join(strParentDir, strDataset, strDataset + "-" +
+                strAligner + ending) for ending in TRIM_ENDINGS]
     else:
-        sam_fnames = [os.path.join(strParentDir, strDataset + "-" + strAligner +
-            NOTRIM_ENDING)]
+        sam_fnames = [os.path.join(strParentDir, strDataset, strDataset + "-" +
+                strAligner + NOTRIM_ENDING)]
 
     return (orig_fnames, trimmed_fnames, sam_fnames)
 
@@ -35,6 +35,13 @@ def countSam(strSamfile):
     iTotalAligned = 0
     iAlignedHuman = 0
     regex = r'\d\d\d\d\d\d_([A-Z][a-z]*)_([a-z]*)_'
+    
+    # determine single end or pair end based on file name
+    single_end = False
+    strLastLetters = strSamfile[-9:]
+    if strLastLetters == TRIM_ENDINGS[1] or strLastLetters == TRIM_ENDINGS[2]:
+        single_end = True
+
     with open(strSamfile, "r") as f:
         for line in f:
             # comment line, skip
@@ -48,6 +55,11 @@ def countSam(strSamfile):
                     if match:
                         if str(match.group(1) + " " + match.group(2)) == 'Homo sapiens': 
                             iAlignedHuman += 1
+    # .sam files for paired ends have one entry for every paired end read
+    if not single_end:
+        iTotalAligned = iTotalAligned/2
+        iAlignedHuman = iAlignedHuman/2
+
     return (iTotalAligned, iAlignedHuman)
 
 def main():
@@ -72,13 +84,13 @@ def main():
     lstrFieldNames = ['OrigTotal', 'OrigHuman', 'TrimTotal', 'TrimHuman',
         'OutTotal', 'OutHuman']
     iLenField = len(lstrFieldNames)
-    iLenDatasets = len(datasets)
+    iLenDatasets = len(args.datasets)
 
     arrResult = np.array([[-1 for i in range(iLenField)] for i in
         range(iLenDatasets)], dtype = "float_")
 
     fnames_all = [getFiles(dataset, args.parent_dir, args.aligner, args.trimmed)
-        for dataset in datasets]
+        for dataset in args.datasets]
 
     for i in range(iLenDatasets):
         (lstr_orig_fnames, lstr_trimmed_fnames, lstr_sam_fnames) = fnames_all[i]
@@ -86,24 +98,27 @@ def main():
         temp_result = np.array([-1 for j in range(iLenField)], dtype="float_")
 
         # get counts for original .fastq files
-        orig_counts = [getCounts.getCounts(fname) for fname in lstr_orig_fnames]
-        temp_result[0] = [f[0] for f in orig_counts]
-        temp_result[1] = [f[1] for f in orig_counts]
+        orig_counts = [getCounts.getCount(fname) for fname in lstr_orig_fnames]
+        temp_result[0] = orig_counts[0][0]
+        temp_result[1] = orig_counts[0][1]
 
         # get counts for trimmed files
         if lstr_trimmed_fnames == []:
-            temp_result[2] = [f[0] for f in orig_counts]
-            temp_result[3] = [f[1] for f in orig_counts]
+            temp_result[2] = temp_result[0]
+            temp_result[3] = temp_result[1]
         else:
-            trim_counts = [getCounts.getCounts(fname) for fname in
+            print(lstr_trimmed_fnames)
+            trim_counts = [getCounts.getCount(fname) for fname in
                 lstr_trimmed_fnames]
-            temp_result[2] = [f[0] for f in trim_counts]
-            temp_result[3] = [f[1] for f in trim_counts]
+            print(trim_counts)
+            temp_result[2] = np.sum([f[0] for f in trim_counts])
+            temp_result[3] = np.sum([f[1] for f in trim_counts])
 
         # get counts for output 
-            out_counts = [countSam(fname) for fname in lstr_sam_fnames]
-            temp_result[4] = np.sum([count[0] for count in out_counts])
-            temp_result[5] = np.sum([count[1] for count in out_counts])
+        out_counts = [countSam(fname) for fname in lstr_sam_fnames]
+        print(out_counts)
+        temp_result[4] = np.sum([count[0] for count in out_counts])
+        temp_result[5] = np.sum([count[1] for count in out_counts])
 
         arrResult[i] = temp_result
 
