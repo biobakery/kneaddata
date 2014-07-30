f71fc66064f19080c7e03fc9197cbd6bcfedcec8
Andy Shi <andyshi@college.harvard.edu>
2014-07-30 15:23:55 -0400

    wrote unified parser and reservoir sampler

---
+++ db_preprocessing/reservoir.py
@@ -0,0 +1,65 @@
+import random
+import argparse
+
+def sample(lElts, iKeep):
+    ''' 
+    Implements reservoir sampling. Keeps iKeep number of things from an lElts
+    iterable.
+    '''
+    if iKeep == 0:
+        return []
+    if iKeep < 0:
+        raise IOError("Must sample a nonnegative number of elements")
+
+    lRes = []
+    iCount = 0
+    for elt in lElts:
+        if iCount < iKeep:
+            lRes.append(elt)
+        else:
+            iRandInd = random.randint(0, iCount-1)
+            if iRandInd < iKeep:
+                lRes[iRandInd] = elt
+        iCount += 1
+    return lRes
+
+def fastaReader(filename):
+    '''
+    Reads a FASTA file. Returns an iterable, whose elements are each entry
+    (header + sequence) in the FASTA file.
+    '''
+    currRead = None
+    with open(filename, "r") as f:
+        for line in f:
+            if line[0] == '>':
+                # that annoying "first read" case
+                if currRead != None:
+                    yield "".join(currRead)
+                currRead = [line]
+            else:
+                currRead.append(line)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--fastas", nargs="+", required=True, 
+            help="Input FASTA files to sample from")
+    parser.add_argument("-o", "--outfiles", nargs="+", required=True,
+            help="Locations to write output")
+    parser.add_argument("-n", "--num-samples", type=int, required=True,
+            help="Number of samples to take")
+
+    args = parser.parse_args()
+
+    if args.num_samples <= 0:
+        raise IOError("Number of samples must be >= 1")
+
+    if len(args.fastas) != len(args.outfiles):
+        raise IOError("Number of input FASTAs must be the same as number of output files")
+
+    for fasta, outfile in zip(args.fastas, args.outfiles):
+        lResult = sample(lElts = fastaReader(fasta), iKeep = args.num_samples)
+        with open(outfile, "w") as f:
+            map(f.write, lResult)
+
+if __name__ == '__main__':
+    main()
---
+++ resultParser.py
@@ -0,0 +1,120 @@
+import re 
+import collections
+
+class ReadCounter():
+    def __init__(self, fileType, pattern=r'.*', combineName=lambda m: m):
+        '''
+        pattern: regex pattern to get the name of the read. Default matches
+        everything. 
+        files: list of files to count
+        fieldNames: list of field names to store
+        fileType: what kind of file are we counting? sam, fastq, etc
+        combineName: a function for combining the regex match
+        '''
+        self.strPattern = pattern
+        self.strFileType = fileType
+
+        # counter for the organisms
+        self.counter = collections.Counter()
+
+        self.combineName = combineName
+
+    def skip(self, strLine):
+        return False
+
+    def processLine(self, strLine):
+        self.counter[strLine] += 1
+
+    def processPairedEnd(self):
+        pass
+
+    def count(self, strFile, bSingleEnd):
+        if strFile[-(len(self.strFileType)):] != self.strFileType:
+            raise Exception(strFile + " is not a " + self.strFileType + " file!")
+
+        try:
+            f = open(strFile, "r")
+            for line in f:
+                if self.skip(line):
+                    pass
+                else:
+                    self.processLine(line)
+        except IOError:
+            print("Could not find file " + strFile)
+            self.iTotalAligned = None
+            self.counter = None
+
+        if not bSingleEnd:
+            self.processPairedEnd()
+
+    def get(self):
+        return self.counter
+
+
+class SamCounter(ReadCounter):
+    def __init__(self, pattern=r'.*', combineName=lambda m: m):
+        ReadCounter.__init__(self, fileType="sam", pattern=pattern,
+                combineName=combineName)
+
+    def skip(self, strLine):
+        return (strLine[0] == '@')
+
+    def processLine(self, strLine):
+        lstrSplitLine = line.split("\t")
+        # alignment!
+        if lstrSplitLine[2].strip() != '*':
+            self.iTotalAligned += 1
+            match = re.search(self.strPattern, lstrSplitLine[0])
+            if match:
+                strOrganism = self.combineName(match)
+                self.counter[strOrganism] += 1
+
+    def processPairedEnd(self):
+        for strOrganism in keys(self.counter):
+            if self.counter[strOrganism] % 2 != 0:
+                raise Exception("Invalid sam file for paired end reads!")
+            self.counter[strOrganism] /= 2
+
+        self.iTotalAligned /= 2
+
+    def get(self):
+        counter = ReadCounter.get(self)
+        return (counter, self.iTotalAligned)
+
+
+class FastqCounter(ReadCounter):
+    def __init__(self, pattern=r'.*', combineName=lambda m: m):
+        ReadCounter.__init__(self, fileType="fastq", pattern=pattern,
+                combineName=combineName)
+        self.iLineCounter = 0
+        self.iLineMod = 4
+        self.iTotalReads = 0
+
+    def skip(self, strLine):
+        # only consider every 4th line
+        bSkip = True
+        if (self.iLineCounter % self.iLineMod) == 0:
+            bSkip = False
+            iLineCounter = 0
+        iLineCounter += 1
+        return bSkip
+
+    def processLine(self, strLine):
+        match = re.search(self.strPattern, strLine)
+        if match:
+            strOrganism = self.combineName(match)
+            self.counter[strOrganism] += 1
+            self.iTotalReads += 1
+
+    def get(self):
+        counter = ReadCounter.get(self)
+        return (counter, self.iTotalReads)
+
+
+class BMTOutCounter(FastqCounter):
+    def __init__(self, pattern=r'.*', combineName=lambda m: m):
+        FastqCounter.__init__(self, fileType="out", pattern=pattern,
+                combineName=combineName)
+
+    def skip(self, strLine):
+        return False
