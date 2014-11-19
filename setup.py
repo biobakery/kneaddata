0caa0ee9637aba90d75eceb94af72164fbb1df2e
rschwager-hsph <rschwager-hsph@localhost>
2014-11-19 14:28:02 +0000

    feature: install via setuptools

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.realpath(os.path.dirname(__file__))
+setup(
+    name='knead-datalib',
+    version='0.0.1',
+    description='',
+    packages=['knead_datalib'],
+    zip_safe=False,
+    install_requires=[
+        'biopython'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+    ],
+    scripts=[os.path.join(here, "knead_data.py")]
+)
