132d6890c1f9f65450edb7084d17df9ac7af1366
Andy Shi <andyshi@college.harvard.edu>
2015-06-11 12:54:32 -0400

    adding library files to execute trf

---
+++ knead_datalib/tandem.py
@@ -0,0 +1,132 @@
+import os
+import logging
+import subprocess
+
+# import stuff from the __init__.py in this directory
+from . import divvy_threads, mktempfifo, process_return
+here = os.path.dirname(os.path.realpath(__file__))
+
+
+def _fastq_to_fasta(fastq, fasta):
+    '''
+    Converts fastq to fasta using fastq_to_fasta.py
+    fastq: Input fastq filename
+    fasta: Output fasta filename
+    '''
+    fastq_to_fasta_cmd = ["python", 
+                          os.path.join(here, "fastq_to_fasta.py"),
+                          fastq, 
+                          "--fasta", fasta]
+    logging.debug("Running fastq_to_fasta with:\n" + str(fastq_to_fasta_cmd))
+    fastq_to_fasta = subprocess.Popen(fastq_to_fasta_cmd, 
+                                    stdout=subprocess.PIPE, 
+                                    stderr=subprocess.PIPE)
+    return(fastq_to_fasta)
+
+
+def _trf(fasta, outfp, match=2, mismatch=7, delta=7, pm=80, pi=10, minscore=50,
+        maxperiod=500, dat=False, mask=False, html=False, trf_path="trf"):
+    '''
+    Generate the TRF command and run it using subprocess. TRF is run such that
+    the reads identified as containing tandem repeats are piped from stdout to
+    another program
+
+    fasta: input fasta filename for TRF
+    outfp: A file pointer that specifies where to write the TRF stdout
+    trf_path: path to trf. If not specified, defaults to "trf"
+
+    match, mismatch, delta, pm, pi, minscore, and maxperiod are all TRF
+    parameters. 
+
+    dat: Tells TRF whether to generate a .dat file
+    mask: Tells TRF whether to generate a mask file.
+    html: Tells TRF whether to generate a HTML output
+    '''
+
+    trf_args = map(str, [match, mismatch, delta, pm, pi, minscore, maxperiod])
+    trf_cmd = [trf_path, fasta] + trf_args + ["-ngs"]
+
+    if mask:
+        trf_cmd += ["-m"]
+    if not html:
+        trf_cmd += ["-h"]
+
+    logging.debug("Running trf with:\n" + str(trf_cmd))
+    trf = subprocess.Popen(trf_cmd, stdout=outfp, stderr=subprocess.PIPE)
+    return(trf)
+
+
+def _generate_fastq(fastq, trf_output, out):
+    cmd = ["python", os.path.join(here, "tandem_process.py"), 
+            fastq, trf_output, out]
+    logging.debug("Running tandem_process.py generate_fastq with:\n" + str(cmd))
+    proc = subprocess.Popen(cmd)
+    return(proc)
+
+
+def _generate_mask(fastq, trf_output, out):
+    cmd = ["python", os.path.join(here, "tandem_process.py"), 
+            fastq, trf_output, out, "--mask"]
+    logging.debug("Running tandem_process.py generate_mask with:\n" + str(cmd))
+    proc = subprocess.Popen(cmd)
+    return(proc)
+    
+
+def _convert(fastq, trf_output, out, mask_fname, generate_fastq, mask):
+    converter_procs = []
+    converter_names = []
+
+    if generate_fastq: 
+        converter_procs.append(_generate_fastq(fastq, trf_output, out))
+        converter_names.append("tandem_process.py generate_fastq")
+
+    if mask:
+        assert(mask_fname != None)
+        converter_procs.append(_generate_mask(fastq, mask_fname,  out))
+        converter_names.append("tandem_process.py generate_mask")
+
+    return(converter_procs, converter_names)
+
+
+def trf(fastq, out, match=2, mismatch=7, delta=7, pm=80, pi=10, minscore=50,
+        maxperiod=500, generate_fastq=True, dat=False, mask=False, html=False,
+        trf_path="trf"):
+
+    fasta_fname = os.path.basename(fastq) + ".fasta"
+    # file name for trf mask (trf default provided, can't change it)
+    trf_args = map(str, [match, mismatch, delta, pm, pi, minscore, maxperiod])
+    trf_out_fname = fasta_fname + "." + ".".join(trf_args) + ".dat"
+    mask_fname = None
+    if mask:
+        mask_fname = fasta_fname + "." + ".".join(trf_args) + ".mask"
+
+    with mktempfifo((fasta_fname, trf_out_fname)) as filenames:
+
+        fastq_to_fasta = _fastq_to_fasta(fastq, filenames[0])
+        
+        trf_out = filenames[1]
+        if dat:
+            trf_out = "something_else"
+
+        converter_procs, converter_names = _convert(fastq, trf_out, out,
+                                                    mask_fname, generate_fastq, 
+                                                    mask)
+        trf_out_fp = open(trf_out, "w")
+        trf = _trf(filenames[0], trf_out_fp, match, mismatch, delta, pm, pi,
+                minscore, maxperiod, dat, mask, html,trf_path)
+        try:
+            procs = [fastq_to_fasta, trf]
+            proc_names = ["fastq_to_fasta " + fastq, "trf " + filenames[0]]
+            for (proc, name) in zip(procs, proc_names):
+                stdout, stderr = proc.communicate()
+                retcode = proc.returncode
+                process_return(name, retcode, stdout, stderr)
+        finally:
+            trf_out_fp.close()
+
+        for (proc, name) in zip(converter_procs, converter_names):
+            stdout, stderr = proc.communicate()
+            retcode = proc.returncode
+            process_return(name, retcode, stdout, stderr)
+
+
---
+++ knead_datalib/tandem_process.py
@@ -0,0 +1,115 @@
+import re
+import sys
+import logging
+import argparse
+from itertools import izip_longest
+
+
+############## constants ######################
+# number of lines per entry in trf output
+TANDEM_LENGTH = 2
+
+# number of lines per entry in fastq 
+FASTQ_LENGTH = 4
+###############################################
+
+
+def generate_fastq(fastq_file, tandem_out_file, out_file):
+    tandem_out = open(tandem_out_file, "r")
+    fastq = open(fastq_file, "r")
+    out = open(out_file, "w")
+
+    try:
+        # read TANDEM_LENGTH lines at a time
+        for tandem_out_lines in izip_longest(*[tandem_out]*TANDEM_LENGTH):
+            # read FASTQ_LENGTH lines at a time
+            for fastq_lines in izip_longest(*[fastq]*FASTQ_LENGTH):
+                # fastq header is NOT a tandem repeat, so keep it
+                if fastq_lines[0] != tandem_out_lines[0]:
+                    try:
+                        out.write("".join(fastq_lines))
+                    except TypeError:
+                        # TypeError can happen when you do 
+                        # "".join([str, str, str, None]). This means that the
+                        # input fastq didn't have 4 lines per read
+                        logging.critical("You probably passed in a bad fastq"
+                                        " file, %s, one that doesn't have 4 lines"
+                                        " per read" % fastq_file)
+                        raise
+    finally:
+        # close everything, even if there was an exception above
+        tandem_out.close()
+        fastq.close()
+        out.close()
+
+
+def generate_masked(fastq_file, masked_file, out_file):
+    masked = open(masked_file, "r")
+    fastq = open(fastq_file, "r")
+    out = open(out_file, "w")
+    fasta_head = re.compile(r'^>')
+
+    def _write_output(out_fp, fastq_header, seq, plus, quals):
+        seq = seq + "\n"
+        assert(len(seq) == len(quals))
+        assert(header[1:] == fastq_header[1:])
+        out_fp.write("".join([fastq_header, seq, plus, quals]))
+
+    try:
+        mask_line = masked.readline()
+        header = None
+        fastq_header = None
+        plus = None
+        quals = None
+        seq = ""
+        for fastq_lines in izip_longest(*[fastq]*FASTQ_LENGTH):
+            seq = ""
+            fastq_header = fastq_lines[0]
+            plus = fastq_lines[2]
+            quals = fastq_lines[3]
+
+            assert(plus.strip() == '+')
+
+            while mask_line:
+                if re.search(fasta_head, mask_line):
+                    if header != None:
+                        _write_output(out, fastq_header, seq, plus, quals)
+                        header = mask_line
+                        mask_line = masked.readline()
+                        break
+                    header = mask_line
+                else:
+                    seq += mask_line.rstrip()
+                mask_line = masked.readline()
+
+        # flush the last guy
+        _write_output(out, fastq_header, seq, plus, quals)
+
+    finally:
+        # close everything if there was an exception above
+        masked.close()
+        fastq.close()
+        out.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fastq", help="Input FASTQ file")
+    parser.add_argument("tandem", help="Input file from TRF")
+    parser.add_argument("output", help="Output file")
+    parser.add_argument("--mask", default=False, action="store_true", 
+            help=("If set, specifies that the input is a masked FASTA. A "
+                 "masked FASTQ will be generated"))
+
+    args = parser.parse_args()
+
+    if args.mask:
+        generate_masked(args.fastq, args.tandem, args.output)
+    else:
+        generate_fastq(args.fastq, args.tandem, args.output)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
