161fd531a598fb301eb42b1d989843a7cc247902
Lauren McIver <Lauren McIver@localhost>
2016-10-05 15:09:05 -0600

    Update version number

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ try:
 except ImportError:
     from urllib import urlretrieve
 
-VERSION="0.5.1"
+VERSION="0.5.2"
 AUTHOR = "KneadData Development Team"
 AUTHOR_EMAIL = "kneaddata-users@googlegroups.com"
 
