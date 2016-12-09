2a9adb14cf055025bc102cab761661725a780502
Lauren McIver <lauren.j.mciver@gmail.com>
2016-12-09 12:44:59 -0700

    Update version

--- kneaddata/knead_data.py
+++ kneaddata/knead_data.py
@@ -74,7 +74,7 @@ except ImportError:
 from kneaddata import run
 from kneaddata import config
 
-VERSION="0.5.2"
+VERSION="0.5.3"
 
 # name global logging instance
 logger=logging.getLogger(__name__)
