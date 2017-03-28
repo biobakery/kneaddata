0e7d1a803dea88351d46bd71c1742a166c1c1d71
Lauren McIver <lauren.j.mciver@gmail.com>
2017-03-27 18:25:57 -0600

    In concat read count script, print the total decontaminated reads, get the file sample name based on the names of the log files (to allow for delimiters in sample names), and determine pair identifier delimiter initially to speed up fastq read

--- kneaddata/read_count_table_concat_pairs.py
+++ kneaddata/read_count_table_concat_pairs.py
@@ -39,17 +39,35 @@ def count_paired_orphan(file):
     sequence_pair1=set()
     sequence_pair2=set()
     orphan=set()
+    print("Reading file: " + file)
+
+    # get the first line of the file to determine the read pair delimiter
+    with open(file) as file_handle:
+        line=file_handle.readline()
+        if line.rstrip()[-2] == "/":
+            delimiter="/"
+        else:
+            delimiter=" "
+    
     for lines in read_file_n_lines(file, 4):
         # split the id to remove the paired identification
         # allow for both space and forward slash to differentiate read pairs
-        if " " in lines[0]:
-            data = lines[0].replace("@","",1).split(" ")
+        if delimiter == " ":
+            data = lines[0].rstrip().replace("@","",1).split(" ")
+            try:
+                pair_set = data[1].split(":")[0]
+            except IndexError:
+                pair_set = "NA"
         else:
-            data = lines[0].replace("@","",1).split("/")
-        
-        if data[1][1] == "1":
+            data = lines[0].rstrip().replace("@","",1).split("/")
+            try:
+                pair_set = data[1]
+            except IndexError:
+                pair_set = "NA"
+   
+        if pair_set == "1":
             sequence_pair1.add(data[0])
-        elif data[1][1] == "2":
+        elif pair_set == "2":
             sequence_pair2.add(data[0])
         else:
             orphan.add(data[0])
@@ -60,7 +78,23 @@ def count_paired_orphan(file):
     orphan1=len(list(sequence_pair1.difference(sequence_pair2)))+len(list(orphan))
     orphan2=len(list(sequence_pair2.difference(sequence_pair1)))
             
+    print("Found counts of pair1, pair2, orphan1, orphan2: " + ",".join(str(i) for i in [pair1, pair2, orphan1, orphan2]))
+
     return pair1, pair2, orphan1, orphan2
+
+def get_sample_name(file, sample_names):
+    """ Figure out the sample name for the file based on the file name and sample names.
+    This allows for multiple delimiters in the sample name. """
+    
+    # get the basename of the file
+    file=os.path.basename(file)
+
+    # get the names of all of the samples, sorted by length with the longest first
+    for name in sorted(sample_names,key=len,reverse=True):
+        if file.startswith(name):
+            return name
+
+    return "Unknown"
 
 def parse_arguments(args):
     """ Parse the arguments from the user"""
@@ -82,7 +116,7 @@ def main():
     trimmed_fastq=filter(lambda x: x.endswith(".fastq") and not "contam" in x and "trimm" in x,files)
     contam_fastq=filter(lambda x: x.endswith(".fastq") and "contam" in x and not "trimm" in x,files)
     final_fastq=filter(lambda x: x.endswith(".fastq") and not "contam" in x and not "trimm" in x,files)
-    
+
     # get the raw single read counts from the log files, this is 2x the number of paired end reads
     reads={}
     for file in logs:
@@ -94,10 +128,10 @@ def main():
                     reads[sample_name]={}
                 reads[sample_name]["raw pair1"]=int(read_count)/2.0
                 reads[sample_name]["raw pair2"]=int(read_count)/2.0
-                
+    
     # get the counts for the trimmed files
     for file in trimmed_fastq:
-        sample_name=os.path.basename(file).split(".")[0]
+        sample_name = get_sample_name(file, reads.keys())
         pair1, pair2, orphan1, orphan2 = count_paired_orphan(file)
         reads[sample_name]["trimmed pair1"]=pair1
         reads[sample_name]["trimmed pair2"]=pair2
@@ -106,18 +140,18 @@ def main():
     
     # get the counts for the contaminate files
     for file in contam_fastq:
-        sample_name = os.path.basename(file).split("_")[0]
-        database_name = os.path.basename(file).split("_")[1]
+        sample_name = get_sample_name(file, reads.keys())
+        database_name = os.path.basename(file).replace(sample_name,"",1).replace("_bowtie2_contam.fastq","")
         pair1, pair2, orphan1, orphan2 = count_paired_orphan(file)
         tag = "decontaminated "+database_name
-        reads[sample_name][tag+" pair1"]=pair1
-        reads[sample_name][tag+" pair2"]=pair2
-        reads[sample_name][tag+" orphan1"]=orphan1
-        reads[sample_name][tag+" orphan2"]=orphan2   
+        reads[sample_name][tag+" pair1"]=reads[sample_name]["trimmed pair1"]-pair1
+        reads[sample_name][tag+" pair2"]=reads[sample_name]["trimmed pair2"]-pair2
+        reads[sample_name][tag+" orphan1"]=reads[sample_name]["trimmed orphan1"]-orphan1
+        reads[sample_name][tag+" orphan2"]=reads[sample_name]["trimmed orphan2"]-orphan2   
         
     # get the counts for the final files
     for file in final_fastq:
-        sample_name=os.path.basename(file).split(".")[0]
+        sample_name = get_sample_name(file, reads.keys())
         pair1, pair2, orphan1, orphan2 = count_paired_orphan(file)
         reads[sample_name]["final pair1"]=pair1
         reads[sample_name]["final pair2"]=pair2
