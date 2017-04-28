220eaf20d70d27f085482f983e73cc9d3bbcaa01
Lauren McIver <lauren.j.mciver@gmail.com>
2017-04-28 13:17:17 -0600

    Update version number

--- kneaddata/knead_data.py
+++ kneaddata/knead_data.py
@@ -74,7 +74,7 @@ except ImportError:
 from kneaddata import run
 from kneaddata import config
 
-VERSION="0.5.4"
+VERSION="0.5.5"
 
 # name global logging instance
 logger=logging.getLogger(__name__)
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ try:
 except ImportError:
     from urllib import urlretrieve
 
-VERSION="0.5.4"
+VERSION="0.5.5"
 AUTHOR = "KneadData Development Team"
 AUTHOR_EMAIL = "kneaddata-users@googlegroups.com"
 
