b59386d34034e117e941f9b2b2ca67049c4e62d9
sagun98 <sagunmaharjann@gmail.com>
2020-05-27 20:26:34 -0400

    Updated code version number

--- kneaddata/knead_data.py
+++ kneaddata/knead_data.py
@@ -74,7 +74,7 @@ except ImportError:
 from kneaddata import run
 from kneaddata import config
 
-VERSION="0.7.6"
+VERSION="0.7.7-alpha"
 
 # name global logging instance
 logger=logging.getLogger(__name__)
