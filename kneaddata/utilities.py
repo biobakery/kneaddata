biobakery/kneaddata

commit ecf589ddfa01402c04b587af9f11d80de0e8f40c

Removed spaces then added identifiers (#25)
Co-authored-by: sagun98 <sagunmaharjann@gmail.com>

kneaddata/utilities.py
@@ -377,18 +377,13 @@ def get_reformatted_identifiers(file, input_index, output_folder, temp_file_list
     with open(new_file, "wt") as file_handle:
         for lines in read_file_n_lines(file,4):
             # reformat the identifier and write to temp file
-            if " 1:" in lines[0]:
-                lines[0]=lines[0].replace(" 1","").rstrip()+"#0/1\n"
-            elif " 2:" in lines[0]:
-                lines[0]=lines[0].replace(" 2","").rstrip()+"#0/2\n"
-            elif " " in lines[0]:
-                if lines[0].endswith("/1\n") or lines[0].endswith("/2\n"):  
-                    lines[0]=lines[0].replace(" ",".")
+            if " " in lines[0]:
+                lines[0]=lines[0].replace(" ",".")
+            if not lines[0].endswith("/1\n") and not lines[0].endswith("/2\n"):  
+                if (input_index == 0):
+                    lines[0]=lines[0].rstrip()+"#0/1\n"
                 else:
-                    if (input_index == 0):
-                        lines[0]=lines[0].replace(" ",".").rstrip()+"#0/1\n"
-                    else:
-                        lines[0]=lines[0].replace(" ",".").rstrip()+"#0/2\n"
+                    lines[0]=lines[0].rstrip()+"#0/2\n"
                     
                     
             file_handle.write("".join(lines))

commit 9ae15e7a7ae3c2a08f3676d87b31355670433e4c

Fix regression introduced by fix for issue #25.
This closes issue #26 which was introduced by commit ecf589d.

kneaddata/utilities.py
@@ -377,15 +377,18 @@ def get_reformatted_identifiers(file, input_index, output_folder, temp_file_list
     with open(new_file, "wt") as file_handle:
         for lines in read_file_n_lines(file,4):
             # reformat the identifier and write to temp file
-            if " " in lines[0]:
+            if " 1:" in lines[0]:
+                lines[0]=lines[0].replace(" 1","")
+            elif " 2:" in lines[0]:
+                lines[0]=lines[0].replace(" 2","")
+            elif " " in lines[0]:
                 lines[0]=lines[0].replace(" ",".")
-            if not lines[0].endswith("/1\n") and not lines[0].endswith("/2\n"):  
+            if not lines[0].endswith("/1\n") and not lines[0].endswith("/2\n"):
                 if (input_index == 0):
                     lines[0]=lines[0].rstrip()+"#0/1\n"
                 else:
                     lines[0]=lines[0].rstrip()+"#0/2\n"
-                    
-                    
+
             file_handle.write("".join(lines))
     
     # add the new file to the list of temp files